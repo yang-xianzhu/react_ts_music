15e3dac0d2955345697e273634c24bf82e5175c8
yang-xianzhu <2416548256@qq.com>
2022-11-27 18:58:17 +0800

    配置代码规范/安装antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Button } from 'antd'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-  return <div className='App'>app</div>;
+  return (
+    <div className="App">
+      <Button type="primary">antd</Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@':resolve(__dirname,'./src')
+    },
+    extensions: [".js", ".json", ".ts"], // 使用路径别名时想要省略的后缀名，可以自己 增减
+  },
+});
